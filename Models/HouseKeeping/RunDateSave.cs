e31306021fbd5809125c83566e27cd259a8c255b
Sridhar Sathya <sridhar_0314@live.com>
2020-10-07 13:46:29 -0400

    Storing Tickers to Queue done.

--- Models/HouseKeeping/RunDateSave.cs
+++ Models/HouseKeeping/RunDateSave.cs
@@ -8,7 +8,8 @@ namespace Models.HouseKeeping
 {
     public class RunDateSave
     {
-		public string ProcessName { get; set; }
+		public string Symbol { get; set; }
+		public string ProcessName { get => Symbol; set => Symbol = value; }
 		public long LastRunTime { get; set; }		
 	}
 }
--- MongoRepository/Model/HouseKeeping/RunDateSaveDb.cs
+++ MongoRepository/Model/HouseKeeping/RunDateSaveDb.cs
@@ -5,12 +5,7 @@ namespace MongoRepository.Model.HouseKeeping
 {
 	[CollectionName("RunDateSave")]
 	public class RunDateSaveDB : RunDateSave, IAPPDoc
-	{
-		public string Symbol 
-		{ 
-			get { return ProcessName; } 
-			set { ProcessName = value; } 
-		}
+	{		
 		public long ComputeDate { get; set; }
 		public string Id { get; set; }
 		public int Version { get; set; }
